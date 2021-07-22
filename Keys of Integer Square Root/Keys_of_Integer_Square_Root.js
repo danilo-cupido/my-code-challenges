6d3c0c80c7f602757c6e4ad5d62ba005ba075f7d
danilo-cupido <danilocupido@gmail.com>
2021-07-22 23:40:40 +0100

    add new challenge

---
+++ Keys of Integer Square Root/Keys_of_Integer_Square_Root.js
@@ -0,0 +1,9 @@
+// Yon need to return an array containing the keys of an object
+// whose value's square root is an integer.
+
+// SOLUTION 1
+
+function squareRootIntegerKeys(obj) {}
+
+console.log(squareRootIntegerKeys({ a: 9, b: 52, c: 4 })); // ['a', 'c']
+console.log(squareRootIntegerKeys({ a: 18, b: 81, c: 65, d: 144, e: 100 })); // ['b', 'd', 'e']
